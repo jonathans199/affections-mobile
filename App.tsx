3dd2fe78dde5bf0f87540001ec4545bc08e49d71
jonathans199 <jso1919@gmail.com>
2023-01-20 14:56:23 -0500

    updated read me with screenshots

--- App.tsx
+++ App.tsx
@@ -1,23 +1,9 @@
-// In App.js in a new project
-
 import * as React from 'react'
-import { View, Text, Button } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import Swiper from 'react-native-swiper'
-
 import Profile from './screens/Profile'
 import Home from './screens/Home'
-
-function HomeScreen({ navigation }: any) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title='test' onPress={() => navigation.navigate('Profile')} />
-    </View>
-  )
-}
 
 const Stack = createNativeStackNavigator()
 
